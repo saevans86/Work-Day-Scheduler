d209099f3f2559f162fbe303d29d2e4073a765ae
saevans86 <sam_evans1986@hotmail.com>
2023-07-20 18:14:13 -0600

    kept prev styling, add addiitional time slot boxes

--- Work-day-scheduler/Develop/script.js
+++ Work-day-scheduler/Develop/script.js
@@ -3,25 +3,24 @@
 // in the html.
 
 
-$(document).ready(function()  {
-  $(".saveBtn").on("click", function(){
-    var time = $(this).attr("id");
-    var textAreaValue = $(this).siblings("textarea").val();
+$(document).ready(function()  { 
+  $(".btn").on("click", function(){
+    var time = $(this).attr("id");  
+    var textAreaValue = $(this).siblings("textarea").val(); 
+
     
-    localStorage.setItem(time, textAreaValue);
+    localStorage.setItem(time, textAreaValue); 
     console.log(textAreaValue, time)
   })
   for (let i = 8; i <= 11; i++) {
-    $(`#${i}`).siblings("textarea").val(localStorage.getItem(i))
+    $(`#${i}`).siblings("textarea").val(localStorage.getItem(i)) 
   }
 
-    var currentTime = new Date().getHours();
-    $()
-
-      $('.time-block').each(function() {
+    var currentTime = new Date().getHours(); 
+          $('.time-block').each(function() {
         var timeSlot = $(this).data('time');
         var time = parseInt(timeSlot);
-        var ampm = timeSlot.split(' ')[1];
+        var ampm = timeSlot.split(' ')[1]; 
 
         if (ampm === 'PM' && time !== 12) {
           time += 12;
@@ -34,45 +33,20 @@ $(document).ready(function()  {
         } else {
           $(this).addClass('future');
         }
-        // const clockDisplay = dayjs().format("hh:mm:ss:a");
-        // $("#currentTime").text(clockDisplay); 
-  })
+ })
 })
+setInterval(function(){
+  const clockDisplay = dayjs().format("hh:mm:ss:a");
+$("#currentTime").text(clockDisplay).text
+})
+
 
 
  
 
-// console.log(clockDisplay)
-// const timeToColor = $("container-fluid").map(function(){ return this.id}).get()
-// const clockDisplay = parseInt( dayjs().format("hh") );
-// $(timeToColor, clockDisplay).toggleClass(function() {
-// $("#currentTime").text(clockDisplay); 
-// for (let index = 0; index < timeToColor.length; index++) {
-//   console.log(timeToColor[index])
-//   // const newColor = timeRows[index].split("-")[1];
-//   // console.log(element)
-// }
-//   if (timeToColor <= clockDisplay) {
-//   $('background-color').css("background-color", "red");
-//   } else if 
-//     (timeToColor == clockDisplay) {
-//       $(timeToColor).removeClass('background-color').addClass("background-color", "black");
-//     }else if 
-//     (timeToColor > clockDisplay) {
-//       $('background-color').css("background-color", "red");
-// $init()
-      
-// }})
-
-// console.log(timeToColor, clockDisplay)
-     
 
 
-  // $(selector).addClass(classname,function(index,currentclass))
 
-//  $("#9").siblings("textarea").val(localStorage.getItem("9"))
-//  $("#10").siblings("textarea").val(localStorage.getItem("10"))
-//  $("#11").siblings("textarea").val(localStorage.getItem("11"))
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
